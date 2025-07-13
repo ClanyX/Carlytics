7cb2834c0ce79a4093e31e58332964d3b1f42ea3
Filip <101202559+ClanyX@users.noreply.github.com>
2025-07-13 14:08:08 +0200

    Add login window and settings management
    Introduces a login window with name and password fields, credential validation, and cancel functionality. Adds application settings for storing credentials, integrates Microsoft.Data.Sqlite package, and creates a placeholder ProgramWindow for post-login navigation.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NPS = Carlytics.Properties.Settings;
 
 namespace Carlytics
 {
@@ -16,9 +19,43 @@ namespace Carlytics
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Functions
+        private string ConvertToUnsecureString(SecureString secureString)
+        {
+            if (secureString == null) return string.Empty;
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void onClickCancel(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void onClickLogin(object sender, RoutedEventArgs e)
+        {
+            if(name.Text == NPS.Default.Name && ConvertToUnsecureString(pass.SecurePassword) == NPS.Default.Password)
+            {
+                ProgramWindow programWindow = new ProgramWindow();
+                programWindow.Show();
+                this.Close();
+            } else 
+                MessageBox.Show("Incorrect credentials", "Error message", MessageBoxButton.OK);
         }
     }
 }
